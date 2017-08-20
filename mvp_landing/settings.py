5ed1dc92162e0f1a4b14ebbfe77c34a7dbac7605
nkushh <mugunapiero@gmail.com>
2017-08-20 07:22:11 +0300

    Added Procfile. Changed settings and wsgi

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e^7vln0f*=np*ux^(976e-2*f18(-%28h*ugap)kdnqzmq3a76'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cowke.herokuapp.com']
 
 
 # Application definition
@@ -128,15 +128,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join((BASE_DIR), 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Uploaded files by the user directory path
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join((BASE_DIR), 'media')
 
 # URL user is redirected to after login success
 LOGIN_REDIRECT_URL = '/'
